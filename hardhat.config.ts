061fc40a3f1e8a1648f020c70fde3b1def37343d
Jake <tjd1emd@naver.com>
2024-10-16 02:05:03 +0900

    Add custom contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
-import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-ignition-ethers";
 import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  networks: {
+    "linea-sepolia": {
+      url: process.env.ALCHEMY_TESTNET_RPC_URL,
+      accounts: [process.env.TESTNET_PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
