dc9e6dc23aeebef7234a576eb741f4387fc2b496
Jake <tjd1emd@naver.com>
2024-10-17 13:36:18 +0900

    Add mocha config

---
+++ contracts/SpecialNumber.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+contract SpecialNumber {
+    address author1;
+    address author2;
+
+    receive() external payable {
+        // msg.value is passed to this contract
+        uint totalValue = msg.value;
+
+        // make a call to author1 sending half of the ether
+        (bool success1, ) = author1.call{ value: totalValue / 2 }("");
+        require(success1);
+
+        // make a call to author2 sending half of the ether
+        (bool success2, ) = author2.call{ value: totalValue / 2 }("");
+        require(success2);
+    }
+}
---
+++ test/SpecialNumber.ts
@@ -0,0 +1,21 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import hre from "hardhat";
+
+describe("SpecialNumber.sol 테스트", () => {
+  async function deploySpecialNumber() {
+    const factory = await hre.ethers.getContractFactory("SpecialNumber");
+    const contract = await factory.deploy();
+    await contract.waitForDeployment();
+    const address = await contract.getAddress();
+
+    console.log("SpecialNumber deployed to:", address);
+
+    return { address };
+  }
+
+  it("[Deployment] 올바른 address를 리턴한다.", async () => {
+    const { address } = await loadFixture(deploySpecialNumber);
+    expect(address).to.length.equal(44);
+  });
+});
